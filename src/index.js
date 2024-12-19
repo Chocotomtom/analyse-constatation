7a6c5d17cce055ab862b9b7ac8e6845836078b67
Chocotomtom <plisson.t@outlook.fr>
2024-12-19 20:44:28 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles.css';  // Ajout des styles
 import App from './App';
 
+const root = document.getElementById('root');
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  root
 );
