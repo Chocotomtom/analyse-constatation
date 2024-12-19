9fca5c0c674cf586e19754254428c777ee9b681c
Chocotomtom <plisson.t@outlook.fr>
2024-12-19 20:37:26 +0100

    Create AnalyseConstatation.js

---
+++ src/components /AnalyseConstatation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AnalyseConstatation = () => {
+  return (
+    <div>
+      <h2 className="text-xl font-bold mb-4">Analyse des Constatations</h2>
+      <p>Contenu à venir...</p>
+    </div>
+  );
+};
+
+export default AnalyseConstatation;
